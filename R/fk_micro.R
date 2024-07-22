e0bdb315ed9a78cc6d48664494c21a11ea93b3ed
Diana Garcia <dana89co@hotmail.com>
2024-07-22 23:35:25 +0200

    fk_micro start documentation and function in data raw

---
+++ R/fk_micro.R
@@ -0,0 +1,21 @@
+#' Micro survey data
+#'
+#' A dataset containing the .... The variables are as follows:
+#'
+#' @format A data frame with xx rows and xx variables:
+#' \describe{
+#'   \item{price}{price in US dollars ($326--$18,823)}
+#'   \item{carat}{weight of the diamond (0.2--5.01)}
+#'   \item{cut}{quality of the cut (Fair, Good, Very Good, Premium, Ideal)}
+#'   \item{color}{diamond colour, from D (best) to J (worst)}
+#'   \item{clarity}{a measurement of how clear the diamond is (I1 (worst), SI2,
+#'     SI1, VS2, VS1, VVS2, VVS1, IF (best))}
+#'   \item{x}{length in mm (0--10.74)}
+#'   \item{y}{width in mm (0--58.9)}
+#'   \item{z}{depth in mm (0--31.8)}
+#'   \item{depth}{total depth percentage = z / mean(x, y) = 2 * z / (x + y) (43--79)}
+#'   \item{table}{width of top of diamond relative to widest point (43--95)}
+#' }
+#'
+#' @source Fake data using average characteristics of old and new surveys at ....
+"fk_micro"
