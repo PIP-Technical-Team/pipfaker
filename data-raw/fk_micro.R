7630b03b69e6eec6644044d8fe3a2ab163c812c8
Diana Garcia <dgarciarojas@worldbank.org>
2024-07-29 03:47:58 -0400

    Make the dataset external and not internal

--- data-raw/fk_micro.R
+++ data-raw/fk_micro.R
@@ -1,14 +1,21 @@
+#Libraries
+
+library(dplyr)
+library(data.table)
+
 # Load pip_invetory
+
 pip_inventory <-
   pipload::pip_find_data(
     inv_file = "//w1wbgencifs01/pip/PIP-Data_QA/_inventory/inventory.fst",
     filter_to_pc = TRUE,
     maindir = "//w1wbgencifs01/pip/PIP-Data_QA/")
 
-library(dplyr)
-library(data.table)
+# Generate micro data
 
 fk_micro <- fk_micro_gen(pip_inventory)
 
+# Save data
+
 #save(fk_micro, file = "data/fk_micro.rda")
-usethis::use_data(fk_micro, internal = TRUE, overwrite = TRUE)
+usethis::use_data(fk_micro, overwrite = TRUE)
