2af095ff14244c4d140c7c82f753bea98b70dd77
Diana Garcia <dgarciarojas@worldbank.org>
2024-09-06 11:27:47 -0400

    New function to load any type of file

--- R/utils.R
+++ R/utils.R
@@ -1,8 +1,8 @@
-#' Load and merge data files from PIP drive
+#' Load data from files in PIP drive
 #'
 #'
-#' @param orig_file a vector of file addresses
-#' (usually `orig` variable from `cache_inventory`)
+#' @param orig_file a file address
+#' (usually `orig` or `cache_file` variable from `cache_inventory`)
 #'
 #' @return list
 #'
@@ -11,7 +11,21 @@ load_files_pip <- function(orig_file) {
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # computations   ---------
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  dt <- lapply(orig_file, haven::read_dta)
+
+  ext <- orig_file |>
+    fs::path_ext()
+
+  if (ext == "qs") {
+    dt <- qs::read(orig_file) |>
+      collapse::qDT()
+  }
+  if (ext == "fst") {
+    dt <- fst::read_fst(orig_file, as.data.table = TRUE)
+  }
+  if (ext == "dta") {
+    dt <- haven::read_dta(orig_file) |>
+      collapse::qDT()
+  }
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Return   ---------
@@ -20,27 +34,27 @@ load_files_pip <- function(orig_file) {
 
 }
 
-#' Load and merge data files from cache
+#' #' Load and merge data files from cache
+#' #'
+#' #'
+#' #' @param cache_file a vector of file addresses
+#' #' (usually `cache_file` variable from `cache_inventory`)
+#' #'
+#' #' @return list
+#' #'
+#' load_files_cache <- function(cache_file) {
 #'
+#'   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#'   # computations   ---------
+#'   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#'   dt <- lapply(cache_file, fst::read_fst)
 #'
-#' @param cache_file a vector of file addresses
-#' (usually `cache_file` variable from `cache_inventory`)
+#'   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#'   # Return   ---------
+#'   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#'   return(dt)
 #'
-#' @return list
-#'
-load_files_cache <- function(cache_file) {
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # computations   ---------
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  dt <- lapply(cache_file, fst::read_fst)
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Return   ---------
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  return(dt)
-
-}
+#' }
 
 
 ignore_unused_imports <- function() {
