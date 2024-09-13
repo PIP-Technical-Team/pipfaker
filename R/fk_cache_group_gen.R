470cd79737815bbc2c0d5a775f235cf579c59c02
Diana Garcia <dgarciarojas@worldbank.org>
2024-09-13 14:22:32 -0400

    Add subfunction for fk_cache_group_gen

--- R/fk_cache_group_gen.R
+++ R/fk_cache_group_gen.R
@@ -13,28 +13,17 @@ fk_cache_group_gen <- function(n_quantiles = 20) {
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
   cache_inventory <- pipload::pip_load_cache_inventory(version = "20240326_2017_01_02_PROD")
-
-  orig_file <- cache_inventory[cache_id=="IND_1983_NSS_D2_CON_GROUP","cache_file"]|>
+  orig_file       <- cache_inventory[cache_id=="IND_1983_NSS_D2_CON_GROUP","cache_file"]|>
     as.character()
 
-  svy_ind <- load_files_pip(orig_file)
-
-  svy_ind_rural <- svy_ind[svy_ind$reporting_level=="rural",]
-
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-  # Variables with unique values   ---------
-  #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-
-  var_dist <- collapse::fndistinct(svy_ind_rural, na.rm = FALSE)
-  uniq     <- var_dist[var_dist==1]
-  var_uniq <- collapse::funique(svy_ind_rural|>
-                                  collapse::fselect(names(uniq)))
+  svy_ind         <- load_files_pip(orig_file)
+  svy_ind_rural   <- svy_ind[svy_ind$reporting_level=="rural",]
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Create new dataset   ---------
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
-  fake_svy <- data.table::as.data.table(var_uniq[rep(1,each=n_quantiles),])
+  fake_svy <- fk_uniq(svy_ind_rural, n_quantiles)
 
   #~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
   # Wealth and Weight   ---------
@@ -44,9 +33,13 @@ fk_cache_group_gen <- function(n_quantiles = 20) {
 
   welfare <- wbpip:::sd_create_synth_vector(datt$lwelfare,datt$weight,mean = 109.90)$welfare
 
-  fake_svy$welfare <- wbpip::md_compute_quantiles(welfare, n_quantile = n_quantiles)
-
-  fake_svy$weight <- 1/n_quantiles
+  fake_svy <- fake_svy[
+    ,
+    welfare := wbpip::md_compute_quantiles(welfare, n_quantile = n_quantiles)
+  ][
+    ,
+    weight  := 1/n_quantiles
+  ]
 
   # As performed in pip_ingestion_pipeline::process_svy_data_to_cache:
 
